44cd40edf538c1e4f5fd64bdf959c717503754b5
Ivan Ryazanov <i.riazanov@napoleonit.ru>
2021-09-30 13:19:39 +0500

    uploading file

--- cgi-bin/result.py
+++ cgi-bin/result.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
-
+import cgi
+import os
 print( "Content-type: text/html")
+
+form = cgi.FieldStorage()
+
+message = None
+
+# Test if the file is loaded for the upload
+if 'image' in form:
+    fileitem = form['image']
+    fn = os.path.basename(fileitem.filename)
+    open(os.path.join(os.getcwd(),'files',fn,), 'wb').write(fileitem.file.read())
+    message = 'The file "' + fn + '" was uploaded successfully'
+else:
+    message = 'No file was uploaded'
+
+
+
 print()
-print("<h1>Hello world!</h1>")
+print("<h1>Hello world!</h1>")  
+print("<p>"+os.getcwd()+"</p>")
+print("<p>"+message+"</p>")
+
 
 
 #chmod a+x result.py
