a691fd7e7530fac7285570a54ce5d7a5a1c108f9
Ivan Ryazanov <i.riazanov@napoleonit.ru>
2021-10-03 23:42:56 +0500

    Unit 9 end

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+import os
+
+from model import process_image
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-   name = request.args.get('username')
-   return  render_template('index.html', name=name)
+    return render_template('index.html')
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    imagefile = request.files.get('image')
+    full_name = os.path.join(os.getcwd(), 'files', imagefile.filename)
+    imagefile.save(full_name)
+    return str(process_image(full_name))
+    
---
+++ model.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+from keras.models import load_model
+
+model = load_model('mnist.h5')
+
+
+def pre_pic(picName):
+    # Сначала откройте входящее исходное изображение
+    img = Image.open(picName)
+    # Меняем размер изображения методом сглаживания
+    reIm = img.resize((28, 28), Image.ANTIALIAS)
+    # Убираем цвета, преобразуем в матрицу
+    im_arr = np.array(reIm.convert("L"))
+    return im_arr
+
+
+def process_image(picName):
+    # предобработка изображения, ресайз и преобразование в серое
+    image = pre_pic(picName)
+    # Меняем размерность на ту, которую принимает модель
+    image = image.reshape((1, 28, 28, 1))
+    # Нормализуем значения цвета 
+    image = image.astype('float32')/255
+    # Получаем ответ от модели
+    predict = model.predict(image)[0].argmax()
+    return predict
